235a6772c24a86aa88cfb2284e3fda0ccc80ef94
RJunandia <rismawan.junandia@wgs.co.id>
2019-09-05 18:14:24 +0700

    Get data from table barang

--- retrofit/getcontact.php
+++ retrofit/getcontact.php
@@ -1,28 +1,35 @@
 <?php
 
-$bid = $_GET['bid'];
+$type = $_GET['item_type'];
+ $type = "barang";
+ if($type == "barang"){
 	
-	require_once('connect.php');
+	require_once 'connect.php';
 	
-	$sql = "SELECT * FROM barang WHERE bid=$bid";
+	$sql = "SELECT * FROM barang";
 	
-	$r = mysqli_query($con,$sql);
+	$r = mysqli_query($conn,$sql);
 	
 	$result = array();
 
-	$row = mysqli_fetch_array($r);
+	while ($row = mysqli_fetch_assoc($r)) {
+		array_push($result,
+             array(
+                 'bid'=>$row['bid'],
+                 'bnama'=>$row['bnama'],
+                 'bsatuan'=>$row['bsatuan'],
+                 'bharga_pokok'=>$row['bharga_pokok'],
+                 'bharga_jual'=>$row['bharga_jual'],
+                 'bharga_jual_grosir'=>$row['bharga_jual_grosir'],
+                 'bstok'=>$row['bstok'],
+                 'btgl_masuk'=>$row['btgl_masuk'],
+                 'bkategori_id'=>$row['bkategori_id']
+             ));
+	}
 	
-	array_push($result,array("bid"=>$row['bid'], 
-							"bnama"=>$row['bnama'], 
-							"bsatuan"=>$row['bsatuan'], 
-                            "bharga_pokok"=>$row['bharga_pokok'],
-                            "bharga_jual"=>$row['bharga_jual'],
-                            "bharga_jual_grosir"=>$row['bharga_jual_grosir'],
-                            "bstok"=>$row['bstok']));
-
-	echo json_encode(array('result'=>$result));
-	
-	mysqli_close($con);
+	echo json_encode($result, TRUE);
+}
+	mysqli_close($conn);
 
 // require_once 'connect.php';
 
