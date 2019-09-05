3607b484f570a5bd1df5119716f8d198d8dabd31
RJunandia <rismawan.junandia@wgs.co.id>
2019-09-05 18:15:20 +0700

    Clean unused comment

--- retrofit/getcontact.php
+++ retrofit/getcontact.php
@@ -30,35 +30,3 @@ $type = $_GET['item_type'];
 	echo json_encode($result, TRUE);
 }
 	mysqli_close($conn);
-
-// require_once 'connect.php';
-
-// $type = $_GET['item_type']; //untuk memeriksa data yang dikirim melalui GET
-// // $type = "barang";
-// if($type == barang){
-        
-//         $query = "SELECT * FROM barang";
-//         $result = mysqli_query($conn, $query);
-
-//         $response = array();
-
-//         while($row = mysqli_fetch_assoc($result)){
-
-//             array_push($response,
-//             array(
-//                 'bid'=>$row['bid'],
-//                 'bnama'=>$row['bnama'],
-//                 'bsatuan'=>$row['bsatuan'],
-//                 'bharga_pokok'=>$row['bharga_pokok'],
-//                 'bharga_jual'=>$row['bharga_jual'],
-//                 'bharga_jual_grosir'=>$row['bharga_jual_grosir'],
-//                 'bstok'=>$row['bstok'],
-//                 'btgl_masuk'=>$row['btgl_masuk'],
-//                 'bkategori_id'=>$row['bkategori_id']
-//             ));
-//         }
-
-//         echo json_encode($response);
-//     }
-
-//     mysqli_close($conn);;
